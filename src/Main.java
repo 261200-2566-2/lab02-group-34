a4277548cc9bee4abb5584cd334f9e1ad9c32e4e
Natan <Th3than@gmail.com>
2023-11-22 21:55:02 +0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,31 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        AirPurifier f47 = new AirPurifier("Promax","077707",9000);
-
         // Use the methods to interact with the AirPurifier object
+        AirPurifier f47 = new AirPurifier("Promax","077707",9000);
         f47.turnOn();
         f47.PMcheck();
         f47.turnOff();
         f47.PMcheck();
         AirPurifier.totalAir();
         AirPurifier.totalsales();
+        System.out.println();
 
-        AirPurifier f48 = new AirPurifier("Pro", "0888088", 1000);
+        AirPurifier f48 = new AirPurifier("Pro", "0888088");
         f48.turnOn();
         f48.PMcheck();
         f48.turnOff();
         f48.PMcheck();
+        // Accessing class methods
+        AirPurifier.totalAir();
+        AirPurifier.totalsales();
+        System.out.println();
+
+        AirPurifier f49 = new AirPurifier("0888088");
+        f49.turnOn();
+        f49.PMcheck();
+        f49.turnOff();
+        f49.PMcheck();
         // Accessing class methods
         AirPurifier.totalAir();
         AirPurifier.totalsales();
